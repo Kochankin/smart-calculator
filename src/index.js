47029e677deb511111d10d79da92703681f2f09d
Marianna <31858221+Kochankin@users.noreply.github.com>
2018-03-18 21:29:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 class SmartCalculator {
   constructor(initialValue) {
     this.storage = [initialValue];
+    this.powersArr = [];
   }
 
   add(number) {
     this.storage.push('+');
     this.storage.push(number);
+    this.powersArr = [];
     this.result = this.storage.join('');
     return this;
   }
@@ -13,6 +15,7 @@ class SmartCalculator {
   subtract(number) {
     this.storage.push('-');
     this.storage.push(number);
+    this.powersArr = [];
     this.result = this.storage.join('');
     return this;
   }
@@ -20,6 +23,7 @@ class SmartCalculator {
   multiply(number) {
     this.storage.push('*');
     this.storage.push(number);
+    this.powersArr = [];
     this.result = this.storage.join('');
     return this;
   }
@@ -27,14 +31,23 @@ class SmartCalculator {
   devide(number) {
     this.storage.push('/');
     this.storage.push(number);
+    this.powersArr = [];
     this.result = this.storage.join('');
     return this;
   }
 
   pow(number) {
-      this.base = this.storage[this.storage.length - 1];
-      this.storage.pop();
-      this.storage.push(Math.pow(this.base, number));
+      this.base = this.storage[this.storage.length - 1]; 
+      if (this.powersArr.length === 0) {
+        this.powersArr.push(this.base); 
+        this.powersArr.push(number);
+      } else {
+       number = Math.pow(this.powersArr[1], number); 
+       this.base = this.powersArr[0]; 
+       this.powersArr[1] = number; 
+      } 
+      this.storage.pop(); 
+      this.storage.push(Math.pow(this.base, number)); 
       this.result = this.storage.join('');
     return this;
   }
